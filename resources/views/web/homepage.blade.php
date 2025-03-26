b2977ca20441dee74d9384a93745bd0497bc1408
nisszhra <nissazahra.212@gmail.com>
2025-03-25 21:10:05 -0700

    [Pertemuan 3] Membuat Layout dan Component

---
+++ resources/views/web/homepage.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h3>Ini adalah halaman Homepage</h3>
+
+    <br>
+    
+    <x-card></x-card>
+
+    <br>
+
+    <x-alert></x-alert>
+
+</x-layout>
---
+++ resources/views/web/single_category.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    
+    <h3>Ini adalah halaman Single Category</h3>
+</x-layout>
